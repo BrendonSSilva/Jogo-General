f54501b711c3984e118bd81b24189d1e62724856
Brendon Silva <brendon.dasilva03@gmail.com>
2022-06-07 23:32:28 -0300

    Primeiro commit

---
+++ README.md
@@ -0,0 +1,33 @@
+# General
+
+Para jogar General são necessários cinco dados comuns (hexaédricos) e uma cartela de marcação. O objetivo do jogo é marcar o maior número de pontos, através de algumas combinações de resultados nos dados.<br><br>
+
+Um jogo de General consiste de um certo número de rodadas: em cada uma delas, cada jogador, por sua vez, joga os dados e, conforme o resultado obtido, marca uma das jogadas previstas em sua cartela. Uma vez marcada, aquela jogada não pode ser repetida pelo mesmo jogador até o final da partida.
+
+# Regras
+1. O jogo consiste em 10 rodadas e cada jogador tem 3 chances em cada rodada. <br>
+
+2. No final de cada rodada, anota-se os pontos no papel em uma tabela que contém os números de 1 a 6, sequência, fula, pôquer e general, além do nome dos participantes. <br>
+
+3. - Sequência: há 3 possibilidades: 1-2-3-4-5 ou -2-3-4-5-6 ou 3-4-5-6-1. Vale 20 pontos.<br>
+
+    - Fula: consistem em 3 dados de um mesmo valor e uma dupla de outro. Ex. 3-3-3-2-2. Vale 30 pontos.<br>
+
+    - Pôquer: 4 dados iguais. Ex. 5-5-5-5-3. Vale 40 pontos.<br>
+
+    - General: os 5 dados iguais. Ex. 6-6-6-6-6. Vale 50 pontos.<br>
+
+    - Números: soma-se o valor dos dados com o mesmo número. Ex. 2-2-2-4-6, marca-se 2+2+2 = 6 no quadrante 2.<br>
+
+4. Quando um jogador faz uma sequência, fula, pôquer ou general na primeira jogada, soma-se 5 pontos a mais. <br>
+
+5. Cada jogador lança os dados uma vez, escolhe o que conservar e tem mais 2 jogadas para decidir o que fazer. <br>
+
+6. Caso o jogador não consiga fazer alguma jogada que lhe resta, ele tem de queimar uma jogada que terá valor zero.<br>
+
+7. Quando todos concluírem as 10 jogadas, soma-se os pontos e quem tiver a maior pontuação ganha o jogo. <br>
+
+# 
+Este é o jogo de tabuleiro favorito da minha namorada, usei isso como base de ideia. <br>
+
+Ainda tenho mudanças para fazer para otimizar o código ;)
---
+++ script.js
@@ -0,0 +1,92 @@
+let d = e => document.querySelector(e);
+var jogadas = 0
+function girar() {
+    let dado1 = Math.floor(Math.random() * 6 + 1)
+    let dado2 = Math.floor(Math.random() * 6 + 1)
+    let dado3 = Math.floor(Math.random() * 6 + 1)
+    let dado4 = Math.floor(Math.random() * 6 + 1)
+    let dado5 = Math.floor(Math.random() * 6 + 1)
+
+    d('.dado1').src = `images/face${dado1}.png`
+
+    d('.dado2').src = `images/face${dado2}.png`
+
+    d('.dado3').src = `images/face${dado3}.png`
+
+    d('.dado4').src = `images/face${dado4}.png`
+
+    d('.dado5').src = `images/face${dado5}.png`
+        jogadas++
+        if(jogadas <= 3) {
+            return d('.jog').innerHTML = `<b>Jogadas: ${jogadas}</b>` 
+        }else if(jogadas===4){
+            jogadas = 1
+            return d('.jog').innerHTML = `<b>Jogadas: ${jogadas}</b>` 
+        } 
+}
+function remover1() {
+    d('.dado1').classList.add('d-none')
+}
+function remover2() {
+    d('.dado2').classList.add('d-none')
+}
+function remover3() {
+    d('.dado3').classList.add('d-none')
+}
+function remover4() {
+    d('.dado4').classList.add('d-none')
+}
+function remover5() {
+    d('.dado5').classList.add('d-none')
+}
+function reiniciar() {
+    document.querySelectorAll('.anotar').forEach(function (e) {
+        e.value = ''
+    })
+    document.querySelectorAll('img').forEach(function (img) {
+        img.classList.remove('d-none')
+    })
+}
+function limpar() {
+    document.querySelectorAll('.col-5').forEach(function (e) {
+        e.value = ''
+    })
+
+}
+
+function somar() {
+    let v1 = d('#n11').value
+    let v2 = d('#n12').value
+    let v3 = d('#n13').value
+    let v4 = d('#n14').value
+    let v5 = d('#n15').value
+    let v6 = d('#n16').value
+    let v7 = d('#n1s').value
+    let v8 = d('#n1f').value
+    let v9 = d('#n1p').value
+    let v10 = d('#n1g').value
+    d('#n1t').value = parseInt(v1, 10) + parseInt(v2, 10) + parseInt(v3, 10) + parseInt(v4, 10) + parseInt(v5, 10) + parseInt(v6, 10) + parseInt(v7, 10) + parseInt(v8, 10) + parseInt(v9, 10) + parseInt(v10, 10)
+
+    let c1 = d('#n21').value
+    let c2 = d('#n22').value
+    let c3 = d('#n23').value
+    let c4 = d('#n24').value
+    let c5 = d('#n25').value
+    let c6 = d('#n26').value
+    let c7 = d('#n2s').value
+    let c8 = d('#n2f').value
+    let c9 = d('#n2p').value
+    let c10 = d('#n2g').value
+    d('#n2t').value = parseInt(c1, 10) + parseInt(c2, 10) + parseInt(c3, 10) + parseInt(c4, 10) + parseInt(c5, 10) + parseInt(c6, 10) + parseInt(c7, 10) + parseInt(c8, 10) + parseInt(c9, 10) + parseInt(c10, 10)
+
+}
+
+
+d('.jogar').addEventListener('click', girar)
+
+d('.limpar').addEventListener('click', limpar)
+
+d('.somar').addEventListener('click', somar)
+
+
+
